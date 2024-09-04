28957f77e50b582c54e0dd821adc3dfc2548cd7d
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-03 23:24:27 -0300

    Sidebar section coded..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Protein Bind: a leading research platform for chemistry research",
+  description: "Chemistry Research Platform",
+};
+
 export default function Home() {
   return (
     <>
-      <div className="h-screen">
+      <DefaultLayout>
         <p>Default Layout</p>
-      </div>
+      </DefaultLayout>
     </>
   );
 }
