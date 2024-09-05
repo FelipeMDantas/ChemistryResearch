228e077d75968a7b8f41f556e9dcfe3aa6c0087f
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-04 23:29:07 -0300

    Dashboard and Header components developed.

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Sidebar from "../Sidebar";
+import Header from "../Header";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -10,9 +11,12 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="relative flex flex-1 flex-col lg:ml-72.5">
-        <div className="mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10">
-          {children}
-        </div>
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <main>
+          <div className="mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </main>
       </div>
     </div>
   );
