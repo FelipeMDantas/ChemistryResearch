baf980b5efe1992f40d946ec23d916b1d350ba74
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-09 23:55:15 -0300

    Connection to NVIDIA NIM structured.

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -2,8 +2,6 @@ import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-console.log(MONGODB_URL);
-
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
