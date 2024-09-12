0e6fd6a05af497a9a413106d7c715bddcd204d1c
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-11 23:34:22 -0300

    Ably provider applied.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,36 @@
 "use client";
+
 import "jsvectormap/dist/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
+import * as Ably from "ably";
+import { AblyProvider, ChannelProvider } from "ably/react";
 import { SessionProvider } from "next-auth/react";
-import { UserProvider } from "./context/UserContext";
+import { UserProvider } from "@/app/context/UserContext";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const client = new Ably.Realtime({
+    key: process.env.NEXT_PUBLIC_ABLY_KEY,
+  });
+
   return (
     <html lang="en">
       <script src="https://unpkg.com/@rdkit/rdkit/dist/RDKit_minimal.js" />
       <body suppressHydrationWarning={true}>
         <SessionProvider>
-          <UserProvider>{children}</UserProvider>
+          <UserProvider>
+            <AblyProvider client={client}>
+              <ChannelProvider channelName="chat-demo1">
+                <div className="font-poppins dark:bg-boxdark-2 dark:text-bodydark ">
+                  {children}
+                </div>
+              </ChannelProvider>
+            </AblyProvider>
+          </UserProvider>
         </SessionProvider>
       </body>
     </html>
